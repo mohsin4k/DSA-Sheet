7e3ed8722ef899aedc6423b983028fbe1f6c578e
unknown <khanmk2000@gmail.com>
2023-04-10 15:50:03 +0530

    reverse an Array

---
+++ arrays/ReverseAnArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class ReverseAnArray {
+	static void rvereseArray(int arr[],
+            int start, int end)
+		{
+			int temp;
+			  
+			while (start < end)
+			{
+			    temp = arr[start];
+			    arr[start] = arr[end];
+			    arr[end] = temp;
+			    start++;
+			    end--;
+			}
+		}   
+}
