7f3ea2ead6a41bd1bbb80ddd69e8c0ce81b50899
unknown <khanmk2000@gmail.com>
2023-04-10 15:20:35 +0530

    KthsmallestElement

--- arrays/KthSmallestElement.java
+++ arrays/KthSmallestElement.java
@@ -1,5 +1,21 @@
 package arrays;
 
 public class KthSmallestElement {
+	
+	 public static int kthSmallest(int[] arr, int l, int r, int k) 
+	    { 
+	         int map[] = new int[100001];
+
+	        for(int i : arr) map[i] = i;
+	        
+	        int count = 0;
+	        for(int i : map) {
+	            if(i>0) {
+	                count++;
+	            }
+	            if(count == k) return i;
+	        }
+	        return -1;
+	    } 
 
 }
